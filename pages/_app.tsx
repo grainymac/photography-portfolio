0e7c08bb88ffa0748838d7c1e7a84399b10439df
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-03-06 14:06:04 -0600

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Expletus_Sans } from '@next/font/google'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
