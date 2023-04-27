21008d322087030947ef52872164804a1ad777be
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-04-26 23:15:25 -0500

    changed images

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,56 +7,48 @@ import Masonry from 'react-masonry-css'
 import classNames from 'classnames'
 
 //import photos
-import photo2 from '../assets/allPhotos/_DSC6476.jpg'
-import photo1 from '../assets/allPhotos/_DSC6294.jpg'
-import photo3 from '../assets/allPhotos/_DSC6865.jpg'
-import photo4 from '../assets/allPhotos/_DSC6943.jpg'
-import photo5 from '../assets/allPhotos/_DSC7003.jpg'
-import photo6 from '../assets/allPhotos/_DSC7143.jpg'
-import photo7 from '../assets/allPhotos/_DSC7191.jpg'
-import photo8 from '../assets/allPhotos/_DSC7295.jpg'
-import photo9 from '../assets/allPhotos/_DSC7898.jpg'
-import photo10 from '../assets/allPhotos/427 (34).jpg'
-import photo11 from '../assets/allPhotos/5620 (8).jpg'
-import photo12 from '../assets/allPhotos/000000180009.jpg'
-import photo13 from '../assets/allPhotos/000000840026.jpg'
-import photo14 from '../assets/allPhotos/000000940011.jpg'
-import photo15 from '../assets/allPhotos/000000950006.jpg'
-import photo16 from '../assets/allPhotos/000002590015.jpg'
-import photo17 from '../assets/allPhotos/000002640002.jpg'
-import photo18 from '../assets/allPhotos/000003680002.jpg'
-import photo19 from '../assets/allPhotos/000003680006.jpg'
-import photo20 from '../assets/allPhotos/000003680007.jpg'
-import photo21 from '../assets/allPhotos/000004220009.jpg'
-import photo22 from '../assets/allPhotos/000004410016.jpg'
-import photo23 from '../assets/allPhotos/000004410029.jpg'
-import photo24 from '../assets/allPhotos/000008740008.jpg'
-import photo25 from '../assets/allPhotos/000008740031.jpg'
-import photo26 from '../assets/allPhotos/000010410012.jpg'
-import photo27 from '../assets/allPhotos/000011880005.jpg'
-import photo28 from '../assets/allPhotos/000011880008.jpg'
-import photo29 from '../assets/allPhotos/000011880020.jpg'
-import photo30 from '../assets/allPhotos/000012230001.jpg'
-import photo31 from '../assets/allPhotos/000013000016.jpg'
-import photo32 from '../assets/allPhotos/000013960005.jpg'
-import photo33 from '../assets/allPhotos/000014380007.jpg'
-import photo34 from '../assets/allPhotos/000016300008.jpg'
-import photo35 from '../assets/allPhotos/000017990010.jpg'
-import photo36 from '../assets/allPhotos/000031200009.jpg'
-import photo37 from '../assets/allPhotos/000040300007.jpg'
-import photo38 from '../assets/allPhotos/000050770007.jpg'
-import photo39 from '../assets/allPhotos/000050780037.jpg'
-import photo40 from '../assets/allPhotos/87840007.jpg'
-import photo41 from '../assets/allPhotos/000092210002.jpg'
-import photo42 from '../assets/allPhotos/A83DB799-2803-41C2-A2FF-18B370288188.jpg'
-import photo43 from '../assets/allPhotos/DSC00238.jpg'
-import photo44 from '../assets/allPhotos/DSC04716.jpg'
-import photo45 from '../assets/allPhotos/F2E7EABD-7019-4CA6-ADBC-A7B0D22809EC.jpg'
-import photo46 from '../assets/allPhotos/IMG_3038.jpg'
-import photo47 from '../assets/allPhotos/IMG_5638.jpg'
-import photo48 from '../assets/allPhotos/IMG_5644.jpg'
-import photo49 from '../assets/allPhotos/IMG_6004.jpg'
-import photo50 from '../assets/allPhotos/IMG_6804.jpg'
+import Lola from '../assets/portraits/_DSC0041.jpg'
+import Girl from '../assets/portraits/_DSC4761.jpg'
+import WitchCat from '../assets/portraits/_DSC4815.jpg'
+import Self from '../assets/portraits/_DSC5576.jpg'
+import Monoride from '../assets/portraits/_DSC6294.jpg'
+import Imitate from '../assets/portraits/_DSC6864.jpg'
+import NoNoMango from '../assets/portraits/_DSC6939.jpg'
+import Sleepdealer from '../assets/portraits/_DSC7298.jpg'
+import Nick from '../assets/portraits/_DSC9209.jpg'
+import Isabella from '../assets/portraits/_DSC9998.jpg'
+import MsD from '../assets/portraits/566CF401-C248-4672-A0BF-744EDEFBFA97.jpg'
+import Guy from '../assets/portraits/000000870009.jpg'
+import SkateKid from '../assets/portraits/000000940009.jpg'
+import PhotoGirl from '../assets/portraits/000000950010.jpg'
+import SkateDude from '../assets/portraits/000002470036.jpg'
+import Charles1 from '../assets/portraits/000004410029.jpg'
+import JournalDude from '../assets/portraits/000012970007.jpg'
+import Keith from '../assets/portraits/000012970009.jpg'
+import Abby from '../assets/portraits/000016300001.jpg'
+import Michelle from '../assets/portraits/20200307-_DSC9385-Edit.jpg'
+import Mia1 from '../assets/portraits/000031150008.jpg'
+import Mia2 from '../assets/portraits/000031160001.jpg'
+import Kevin1 from '../assets/portraits/000040300003.jpg'
+import Kevin2 from '../assets/portraits/000040300007.jpg'
+import BikerDude from '../assets/portraits/000050760004.jpg'
+import Anthony from '../assets/portraits/000050770008.jpg'
+import Miles1 from '../assets/portraits/000057300001.jpg'
+import Miles2 from '../assets/portraits/DSC00317.jpg'
+import SkullGirl1 from '../assets/portraits/87840016.jpg'
+import SkullGirl2 from '../assets/portraits/IMG_5752.jpg'
+import Starr from '../assets/portraits/000092210005.jpg'
+import Eryn1 from '../assets/portraits/000092220002.jpg'
+import Eryn2 from '../assets/portraits/000092220003.jpg'
+import Mason from '../assets/portraits/000094100007.jpg'
+import Tyler1 from '../assets/portraits/000390450015.jpg'
+import Tyler2 from '../assets/portraits/IMG_3196.jpg'
+import Angela1 from '../assets/portraits/E4D38FA1-D63D-467D-A0A5-52DBF723FBE9.jpg'
+import Angela2 from '../assets/portraits/E34AF013-35DF-4F3D-8688-955ED00501AD.jpg'
+import Kara from '../assets/portraits/IMG_1441.jpg'
+import Paige from '../assets/portraits/IMG_2244.jpg'
+
+
 
 import Ianmac from '/public/ianmac.png'
 
@@ -82,56 +74,46 @@ const tabs = [
   },
 ]
 const images = [
-  photo6,
-  photo2,
-  photo3,
-  photo4,
-  photo11,
-  photo1,
-  photo5,
-  photo7,
-  photo9,
-  photo10,
-  photo12,
-  photo8,
-  photo18,
-  photo13,
-  photo15,
-  photo16,
-  photo17,
-  photo14,
-  photo19,
-  photo20,
-  photo21,
-  photo22,
-  photo29,
-  photo24,
-  photo25,
-  photo23,
-  photo26,
-  photo34,
-  photo28,
-  photo31,
-  photo27,
-  photo32,
-  photo33,
-  photo30,
-  photo35,
-  photo36,
-  photo37,
-  photo43,
-  photo39,
-  photo40,
-  photo41,
-  photo38,
-  photo42,
-  photo49,
-  photo44,
-  photo46,
-  photo47,
-  photo48,
-  photo45,
-  photo50
+  Self,
+  MsD,
+  Lola,
+  Monoride,
+  Girl,
+  WitchCat,
+  Imitate,
+  NoNoMango,
+  SkateDude,
+  PhotoGirl,
+  Sleepdealer,
+  Nick,
+  Abby,
+  Isabella,
+  Guy,
+  SkateKid,
+  Keith,
+  Kara,
+  Paige,
+  Kevin1,
+  Kevin2,
+  Starr,
+  Charles1,
+  JournalDude,
+  Michelle,
+  Mia1,
+  Mia2,
+  Mason,
+  Miles1,
+  Miles2,
+  Angela2,
+  Angela1,
+  SkullGirl1,
+  SkullGirl2,
+  BikerDude,
+  Eryn1,
+  Eryn2,
+  Tyler1,
+  Tyler2,
+  Anthony
 ]
 
 export default function Home() {
@@ -140,7 +122,7 @@ export default function Home() {
   return (
     <div className='flex flex-col h-full bg-black overflow-auto'>
       <Head>
-        <title>Ian McIntosh - Photography Portfolio</title>
+        <title>Ian Mac Photography</title>
         <meta name="description" content="Generated by create next app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/camera.png" />
@@ -171,7 +153,7 @@ export default function Home() {
             </Tab.List>
             <Tab.Panels className='h-full max-w-[900px] w-full p-2 sm:p-4 my-6'>
               <Tab.Panel className=''>
-                <Masonry breakpointCols={2} className='flex gap-4' columnClassName=''>
+                <Masonry breakpointCols={3} className='flex gap-4' columnClassName=''>
                   
                   {images.map((image, idx) => (
                     <Image 
